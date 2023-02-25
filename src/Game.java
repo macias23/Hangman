8c082fd771588d1bf1f134e3a7c221454831fd8e
Maciej Lipski <maciej.lipski2.stud@pw.edu.pl>
2023-02-25 11:44:45 +0100

    Usunięcie dodawania się wiele razy tych samych liter do tablicy wykorzystanych.

--- src/Game.java
+++ src/Game.java
@@ -2,21 +2,19 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.util.Dictionary;
-import java.util.Scanner;
 
 public class Game extends PrepareGame {
     private final char[] gameInProgress;
     private int numberOfTry;
-    private ArrayList<Character> usedLetters;
+    private HashSet<Character> usedLetters;
     private GUI gui;
     Game()throws IOException {
         super("dictionary");
         gameInProgress = emptyGame;
         numberOfTry=1;
-        usedLetters = new ArrayList<Character>();
+        usedLetters = new HashSet<Character>();
         gui = new GUI("wisielec");
         String displayedText2 = ("Próba nr "+numberOfTry);
         String displayedText1 = "Plansza do gry: " + Arrays.toString(gameInProgress) + " " +
@@ -28,26 +26,25 @@ public class Game extends PrepareGame {
 
     }
 
-    public void gameLoop() {
-
-        while (!Arrays.equals(wordToBeGuessed,gameInProgress)) {
-            gui.textField.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    String typed = gui.textField.getText();
-                    char typedChar = typed.charAt(0);
-                    gui.textField.setText("");
-                    if (!(letterPresence(typedChar, wordToBeGuessed, gameInProgress))) usedLetters.add(typedChar);
-                    String displayedText1 = "Plansza do gry: " + Arrays.toString(gameInProgress) + " " +
-                            "wykorzystane litery: "+usedLetters;
-                    numberOfTry++;
-                    String displayedText2="Próba nr "+numberOfTry;
-                    gui.board.setText("<html>"+displayedText1+"<br>"+displayedText2+"</html>");
-                }
-            });
+    public void gameLoop() throws IOException{
+            while (!Arrays.equals(wordToBeGuessed, gameInProgress)) {
+                gui.textField.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String typed = gui.textField.getText();
+                        char typedChar = typed.charAt(0);
+                        gui.textField.setText("");
+                        if (!(letterPresence(typedChar, wordToBeGuessed, gameInProgress))) usedLetters.add(typedChar);
+                        String displayedText1 = "Plansza do gry: " + Arrays.toString(gameInProgress) + " " +
+                                "wykorzystane litery: " + usedLetters;
+                        numberOfTry++;
+                        String displayedText2 = "Próba nr " + numberOfTry;
+                        gui.board.setText("<html>" + displayedText1 + "<br>" + displayedText2 + "</html>");
+                    }
+                });
+            }
+            gui.board.setText("Wygrana w " + numberOfTry + " próbach! Słowo do zgadnięcia to " + String.valueOf(wordToBeGuessed));
         }
-        gui.board.setText("Wygrana w " + numberOfTry +" próbach! Słowo do zgadnięcia to " + String.valueOf(wordToBeGuessed));
-    }
 
 
     public boolean letterPresence(char x, char[] fullWord, char[] game) {
